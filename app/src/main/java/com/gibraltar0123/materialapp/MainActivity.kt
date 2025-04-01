bee0aac7ff856e4f615a49fa51c03291be43c043
Muhamad <ramlahhendri123@gmail.com>
2025-04-01 18:48:51 +0700

    menambahkan checkbox untuk select all dan check box biasa(belum bisa di view)

--- app/src/main/java/com/gibraltar0123/materialapp/MainActivity.kt
+++ app/src/main/java/com/gibraltar0123/materialapp/MainActivity.kt
@@ -4,27 +4,39 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.gibraltar0123.materialapp.model.MaterialOption
 import com.gibraltar0123.materialapp.ui.theme.MaterialAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialAppTheme {
-                MainScreen()
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    MainScreen()
+                }
             }
         }
     }
@@ -48,8 +60,7 @@ fun MainScreen() {
             )
         }
     ) { innerPadding ->
-        Greeting(
-            name = "Android",
+        CheckboxParentExample(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
@@ -58,12 +69,46 @@ fun MainScreen() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CheckboxParentExample(modifier: Modifier = Modifier) {
+
+    val materialOptions = listOf(
+        MaterialOption(name = "Semen", imageResId = R.drawable.semen),
+        MaterialOption(name = "Kayu", imageResId = R.drawable.kayu),
+        MaterialOption(name = "BatuBata", imageResId = R.drawable.batamerah)
     )
-}
+
+
+    val childCheckedStates = remember { mutableStateListOf(false, false, false) }
+
+    val parentState = when {
+        childCheckedStates.all { it } -> androidx.compose.ui.state.ToggleableState.On
+        childCheckedStates.none { it } -> androidx.compose.ui.state.ToggleableState.Off
+        else -> androidx.compose.ui.state.ToggleableState.Indeterminate
+    }
+
+    Column(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text("Select all")
+            TriStateCheckbox(
+                state = parentState,
+                onClick = {
+                    val newState = parentState != androidx.compose.ui.state.ToggleableState.On
+                    childCheckedStates.forEachIndexed { index, _ ->
+                        childCheckedStates[index] = newState
+                    }
+                }
+            )
+        }
+
+
+
+                    }
+
+            }
+
+
 
 @Preview(showBackground = true)
 @Composable
