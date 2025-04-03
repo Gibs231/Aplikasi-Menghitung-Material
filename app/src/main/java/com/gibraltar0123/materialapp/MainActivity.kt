cdb076963b5037746601ab7fe2643b116f4bdd83
Muhamad <ramlahhendri123@gmail.com>
2025-04-03 17:13:06 +0700

    sanity check

--- app/src/main/java/com/gibraltar0123/materialapp/MainActivity.kt
+++ app/src/main/java/com/gibraltar0123/materialapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -15,8 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gibraltar0123.materialapp.ui.theme.MaterialAppTheme
 import com.gibraltar0123.materialapp.model.MaterialOption
 
@@ -121,6 +124,7 @@ fun CheckboxParentExample(modifier: Modifier = Modifier) {
 
                 if (childCheckedStates[index]) {
                     var text by remember { mutableStateOf(packageCounts[index].toString()) }
+                    var isError by remember { mutableStateOf(false) }
 
                     TextField(
                         value = text,
@@ -128,16 +132,36 @@ fun CheckboxParentExample(modifier: Modifier = Modifier) {
                             if (newText.isEmpty() || newText.toIntOrNull() != null) {
                                 text = newText
                                 val number = newText.toIntOrNull()
-                                if (number != null && number in 1..10000) {
-                                    packageCounts[index] = number
-                                } else if (newText.isEmpty()) {
-                                    packageCounts[index] = 0
+
+                                when  {
+                                    number == null -> {
+                                        packageCounts[index] = 0
+                                        isError = false
+                                    }
+                                    number in 1..10000 -> {
+                                        packageCounts[index] = number
+                                        isError = false
+                                    }
+                                    else -> {
+                                        isError = true
+                                    }
                                 }
                             }
                         },
                         label = { Text(stringResource(id = R.string.package_count_label)) },
-                        modifier = Modifier.width(180.dp)
+                        modifier = Modifier.width(180.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        isError = isError // Tambahkan indikator error
                     )
+
+
+                    if (isError) {
+                        Text(
+                            text = stringResource(id = R.string.input_exceeded_total_package),
+                            color = Color.Red,
+                            fontSize = 12.sp
+                        )
+                    }
                 }
             }
         }
